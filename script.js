09a2fa4bebddb692e9de16b5fc3698f6135ff281
Sultan Bakirov <sultanbek.bakirov.kk@gmail.com>
2024-08-14 06:15:05 +0200

    Age Calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+let currrentYear = 2024;
+let birthYear = 2001;
+
+let age1 = currrentYear - birthYear;
+let age2 = age1 + 1;
+
+let ageOutput = `They are either ${age1} or ${age2}`;
+
+document.getElementById("age").textContent = ageOutput;
